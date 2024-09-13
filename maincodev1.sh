cb87834250bc2c5f28fccb53d997a9a20e02065c
nesty <ernesto.herbas@gmail.com>
2024-09-13 14:05:36 +0800

    added case-insensitive feature for input in team

--- maincodev1.sh
+++ maincodev1.sh
@@ -9,6 +9,9 @@ while true; do
 	  read -p "Shift: " varsched
 	  read -p "Team: " varteam
 	fi
+
+		varteam=$(echo "$varteam" | tr '[:lower:]' '[:upper:]')
+
 		case $varteam in A1|A2|A3|B1|B2|B3)
 			echo $varname $varsched $varteam ;;
 			*)
